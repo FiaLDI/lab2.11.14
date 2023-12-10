0849579e5b79400f0dc307e56c7217b5bfc808fc
FiaLDI <leon.youtube2017@yandex.ru>
2023-12-10 21:42:23 +0300

    code

--- prog/ind.py
+++ prog/ind.py
@@ -1,10 +1,21 @@
 
 def search_area(type = 0):
+    '''
+    Функция вычисления плозади.
+    '''
     def triangle(a, h):
+        '''
+        Функция вычисления площади треугольника.
+        '''
         return 1 / 2 * a * h
 
+
     def rectangle(a, b):
+        '''
+        Функция вычисления площади прямоугольника.
+        '''
         return a * b
+    
     
     if type == 0:
         return triangle
@@ -13,8 +24,12 @@ def search_area(type = 0):
 
 
 def main():
-    print(search_area(0)(1,2))
-    print(search_area(1)(1,2))
+    '''
+    Главная функция программы.
+    '''
+    print(search_area(0)(1, 2))
+    print(search_area(1)(1, 2))
+
 
 if __name__ == "__main__":
     main()
